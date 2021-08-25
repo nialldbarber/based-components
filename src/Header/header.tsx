132f9fa704664766a799e9b40053e44726f30fac
Niall Barber <niall.barber@actual-experience.com>
2021-08-25 16:17:38 +0100

    Updated header

--- src/Header/header.tsx
+++ src/Header/header.tsx
@@ -1,14 +1,14 @@
 import React, {ReactNode, ReactElement, CSSProperties} from 'react';
 import {BasedHeader} from './styles';
 
-export const tuple = <T extends string[]>(...args: T) => args;
-export const tupleNum = <T extends number[]>(...args: T) => args;
+export const stringTuple = <T extends string[]>(...args: T) => args;
+export const numberedTuple = <T extends number[]>(...args: T) => args;
 
-const TITLE_ELE_LIST = tupleNum(1, 2, 3, 4, 5);
+const HEADER_LIST = numberedTuple(1, 2, 3, 4, 5);
 
 export type HeaderPropsT = {
   text?: string;
-  level: typeof TITLE_ELE_LIST[number];
+  level: typeof HEADER_LIST[number];
   weight?: CSSProperties | any; // todo: <- figure out this type
   strong?: boolean;
   line?: boolean;

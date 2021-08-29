295dc7e667269f4569d888246a4b95885c2d5e18
Niall Barber <niall.barber@actual-experience.com>
2021-08-29 11:03:50 +0100

    Added accessibility to sb testing

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,11 @@
 module.exports = {
-  "stories": [
-    "../storybook/**/*.stories.mdx",
-    "../storybook/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: [
+    '../storybook/**/*.stories.mdx',
+    '../storybook/**/*.stories.@(js|jsx|ts|tsx)',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
   ],
-}
+};

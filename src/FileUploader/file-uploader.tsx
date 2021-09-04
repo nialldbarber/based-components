d6fcba9f812aa9fc8b9445a63218943d11d11e88
Niall Barber <niall.barber@actual-experience.com>
2021-09-04 11:16:12 +0100

    Moved file uploader styles into own file

--- src/FileUploader/file-uploader.tsx
+++ src/FileUploader/file-uploader.tsx
@@ -1,9 +1,5 @@
 import React, {forwardRef, ReactElement, CSSProperties} from 'react';
-import styled from 'styled-components';
-import Button from '../Button/button';
-import {getButtonStyles} from '../Button/styles';
-import {b} from '../system/based-provider';
-import {getBaseDefaults} from '../system/base-mixins';
+import {BasedFileUploader, UploaderContainer, Uploader} from './styles';
 
 export type FileUploaderPropsT = {
   uploadMessage?: string;
@@ -11,53 +7,6 @@ export type FileUploaderPropsT = {
   className?: string;
   customStyles?: CSSProperties;
 };
-
-const UploaderContainer = styled.div`
-  position: absolute;
-  top: calc(50% - 1.5rem);
-  left: 50%;
-  transform: translate(-50%, -50%);
-
-  p {
-    color: var(--${b}-white-one);
-    font-size: 20px;
-    font-weight: 700;
-    white-space: nowrap;
-  }
-`;
-
-const BasedFileUploader = styled.div<FileUploaderPropsT>`
-  ${getBaseDefaults};
-  position: relative;
-  background: var(--${b}-white-hovered);
-  height: 250px;
-  width: 400px;
-  border-radius: 12px;
-  border: 6px dashed var(--${b}-white-dark);
-`;
-
-const Uploader = styled.div`
-  position: relative;
-  padding: 0 4rem;
-  margin: 0 auto;
-  max-width: 100px;
-
-  label {
-    ${getButtonStyles};
-    position: absolute;
-    left: 0;
-    width: 100%;
-    height: 100%;
-
-    span {
-      font-weight: 600;
-    }
-  }
-
-  input[type='file'] {
-    display: none;
-  }
-`;
 
 const FileUploader = forwardRef<HTMLDivElement, FileUploaderPropsT>(
   (
